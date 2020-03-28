bd4cf50e2dc622b4c96ffff064d478b4ccdd921a
Darrick Pang <dkypang@yahoo.com>
2020-03-28 02:14:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,6 +9,7 @@ def directors_totals(nds)
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   
   result = {
+    
   }
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
@@ -20,7 +21,7 @@ def directors_totals(nds)
   #
   # Be sure to return the result at the end!
   pp nds
-  nil
+  return result 
 end
 
 def sum 
